7857197de374145869cd763e11bc349acfcfcdc5
yasky6 <kristophermartin2@gmail.com>
2025-08-20 17:52:12 -0700

    Remove Stripe API version to use default

--- src/app/api/stripe/portal/route.ts
+++ src/app/api/stripe/portal/route.ts
@@ -1,9 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-10-28.acacia',
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST(req: NextRequest) {
   try {
